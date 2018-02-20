460e93d767c9edae1fc4534c738b3432b52ed356
agrha <agrhakartz@gmail.com>
2018-02-20 16:09:06 +0700

    agrha

--- controllers.js
+++ controllers.js
@@ -4,15 +4,26 @@ const timer = seconds =>
   new Promise((resolve, reject) => {
     const now = Date.now();
     const then = now + seconds * 1000;
-
-    displayCountdown(seconds - 1);
-    setInterval(() => {
-      const secondsLeft = Math.floor((then - Date.now()) / 1000);
+    let count = setInterval(() => {
+      var secondsLeft = Math.ceil(((then - Date.now()) / 1000)+1);
+      let minutes = Math.floor(secondsLeft/60)
+      let detik = secondsLeft%60
       if (secondsLeft < 0) {
-        clearInterval();
+        clearInterval(count);
+        displayCountdown('BOOM')
         resolve();
+      } else if(minutes < 10 && detik <10){
+        displayCountdown('0'+minutes+':0'+detik)
+        secondsLeft -= 1
+      } else if (minutes < 10){
+        displayCountdown('0'+minutes+':'+detik)
+        secondsLeft -= 1
+      } else if (detik <10){
+        displayCountdown(minutes+':0'+detik)
+        secondsLeft -= 1
       } else {
-        displayCountdown(secondsLeft);
+        displayCountdown(minutes+':'+detik)
+        secondsLeft -= 1
       }
     }, 1000);
   });
