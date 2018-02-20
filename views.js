67479c4c5dd3718ca7e3233846bd3d19af43385f
Fitrul Islam <fitrul.islam@gmail.com>
2018-02-20 18:25:58 +0700

    done(?)

--- views.js
+++ views.js
@@ -4,8 +4,24 @@ const chalk = require("chalk");
 
 const displayCountdown = seconds => {
   clear();
-  console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  let menit = String(Math.floor(seconds/60));
+  let detik = String(seconds%60);
+  if(menit.length===1){
+    var formatMenit = `0${menit}`;
+  } else {
+    formatMenit = `${menit}`;
+  }
+  if(detik.length===1){
+    var formatDetik = `0${detik}`;
+  } else {
+    formatDetik = `${detik}`;
+  }
+  console.log(chalk.cyan(figlet.textSync(`Countdown App`)));
+  if(seconds>=0){
+    console.log(chalk.cyan(figlet.textSync(`                        ${formatMenit}:${formatDetik}`)));
+  } else {
+    console.log(chalk.cyan(figlet.textSync(`                    Selesai!`)));
+  }
 };
 
 module.exports = { displayCountdown };
