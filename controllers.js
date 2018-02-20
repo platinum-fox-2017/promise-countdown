ac469c9aa7947c50b2bf57b4b3079c89b61c3e2f
Reza Pramudhika <rezapramudhika@gmail.com>
2018-02-20 15:28:01 +0700

    Release 0

--- controllers.js
+++ controllers.js
@@ -6,11 +6,10 @@ const timer = seconds =>
     const then = now + seconds * 1000;
 
     displayCountdown(seconds - 1);
-    setInterval(() => {
+    var interval = setInterval(() => {
       const secondsLeft = Math.floor((then - Date.now()) / 1000);
       if (secondsLeft < 0) {
-        clearInterval();
-        resolve();
+        resolve(interval);
       } else {
         displayCountdown(secondsLeft);
       }
