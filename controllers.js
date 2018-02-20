2554544c1faa610b360bd2301b156e3743db1e5a
agnynureza <rezaagny@gmail.com>
2018-02-20 19:59:11 +0700

    promise

--- controllers.js
+++ controllers.js
@@ -4,14 +4,29 @@ const timer = seconds =>
   new Promise((resolve, reject) => {
     const now = Date.now();
     const then = now + seconds * 1000;
-
-    displayCountdown(seconds - 1);
-    setInterval(() => {
-      const secondsLeft = Math.floor((then - Date.now()) / 1000);
-      if (secondsLeft < 0) {
-        clearInterval();
+    let count = seconds 
+    // displayCountdown(seconds - 1);
+    let countdown = setInterval(() => {
+      let secondsLeft = Math.floor((then - Date.now()) / 1000)+2;
+      let minute = Math.floor(secondsLeft/60)
+      let second = secondsLeft%60
+      if(second < 10 && minute === 0){
+        secondsLeft = ` 00 : 0${second} `
+      } else if (second >= 10 && minute == 0){
+        secondsLeft = `00 : ${second}`
+      }else if (second < 10  && minute < 10){
+        secondsLeft = `0${minute} : 0${second}`
+      }else if(second >= 10  && minute < 10){
+        secondsLeft =`0${minute} : ${second}`
+      }else{
+        secondsLeft = `${minute} : ${second}`
+      }
+      if (count === -1) {
+        clearInterval(countdown);
         resolve();
       } else {
+        count--
+        console.log(count)
         displayCountdown(secondsLeft);
       }
     }, 1000);
