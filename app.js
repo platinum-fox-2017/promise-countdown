5973be2c03a6ff5467434af4067b0c3e7555260b
agungcaproex <agung.caproex@gmail.com>
2018-02-20 21:53:22 +0700

    not perfect

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const { timer } = require("./controllers");
 const main = () => {
   const seconds = parseInt(process.argv[2]) || 10;
   // Your code here...
+  timer(seconds).then(function(interval){
+    clearInterval(interval)
+  }).catch(function(err){
+    console.log(err)
+  })
 };
 
 main();
