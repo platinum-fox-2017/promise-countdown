c3ff4e25ca9c3a6665a7106a0c94fe8e9d4ab254
Fadhilmch <fadhil.mochammad1095@gmail.com>
2018-02-20 16:10:40 +0700

    finished

--- controllers.js
+++ controllers.js
@@ -5,17 +5,25 @@ const timer = seconds =>
     const now = Date.now();
     const then = now + seconds * 1000;
 
-    displayCountdown(seconds - 1);
+    displayCountdown(second_to_string(seconds - 1));
     setInterval(() => {
       const secondsLeft = Math.floor((then - Date.now()) / 1000);
       if (secondsLeft < 0) {
         clearInterval();
         resolve();
       } else {
-        displayCountdown(secondsLeft);
+        displayCountdown(second_to_string(secondsLeft));
       }
     }, 1000);
   });
+
+
+
+  function second_to_string(seconds) {
+      let minuteStr = Math.floor(seconds / 60);
+      let secondStr = seconds % 60;
+      return ((minuteStr < 10) ? ((minuteStr = 0)?'00':'0'+minuteStr) : minuteStr) + " : " + ((secondStr<10)?'0'+secondStr:secondStr) ;
+  }
 
 module.exports = {
   timer
