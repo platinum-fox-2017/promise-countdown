791aa29e393c9861bfdb812fef86073959fc6b5c
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-02-20 15:03:55 +0700

    Release 0: Change Callback to promise

--- views.js
+++ views.js
@@ -5,7 +5,19 @@ const chalk = require("chalk");
 const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  let menit = seconds / 60;
+  menit = Math.floor(menit);
+  if (menit < 1) {
+    menit = "00";
+  } else if (String(menit).length == 1){
+    menit = `0${menit}`;
+  }
+  let sisaDetik = seconds % 60;
+  if (String(sisaDetik).length === 1) {
+    sisaDetik = `0${sisaDetik}`;
+  }
+  let waktu = `${menit}:${sisaDetik}`;
+  console.log(chalk.cyan(figlet.textSync(waktu)));
 };
 
 module.exports = { displayCountdown };
