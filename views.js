754300232a44160641984e1c59f51ee8cc309495
haripermadi <harinurpermadi@gmail.com>
2018-02-20 17:28:46 +0700

    release 0

--- views.js
+++ views.js
@@ -4,8 +4,13 @@ const chalk = require("chalk");
 
 const displayCountdown = seconds => {
   clear();
-  console.log(chalk.cyan(figlet.textSync("Countdown App")));
+  console.log(chalk.cyan(figlet.textSync("Countdown App",{
+    font : 'Banner'
+  })));
   // Your code here...
+  console.log(chalk.redBright(figlet.textSync(seconds,{
+    font : 'Acrobatic'
+  })))
 };
 
 module.exports = { displayCountdown };
