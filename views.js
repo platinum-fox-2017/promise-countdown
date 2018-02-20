803f174ec4cc584b681a64995f6681688bb8ca7a
marcosumali <marco.sumali90@gmail.com>
2018-02-20 19:49:59 +0700

    add hasil promise

--- views.js
+++ views.js
@@ -6,6 +6,21 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  var menit = Math.floor(seconds / 60);
+  var detik = seconds - (menit * 60);
+
+  if (menit < 10) {
+    menit = '0' + menit;
+  } 
+
+  if (detik < 10) {
+    detik = '0' + detik;
+  }
+
+  console.log(chalk.green(figlet.textSync(`${menit}:${detik}`)));  
+
+
+
 };
 
 module.exports = { displayCountdown };
