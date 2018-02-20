4353674abf5f6f4403e310aeecd77b729cd53724
Fitrul Islam <fitrul.islam@gmail.com>
2018-02-20 21:06:21 +0700

    done

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const { timer } = require("./controllers");
 
 const main = () => {
   const seconds = parseInt(process.argv[2]) || 10;
-  timer(seconds);
+  timer(seconds)
+    .then(function(seconds){})
+    .catch(function(err){});
 };
 
 main();
