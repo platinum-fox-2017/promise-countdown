5fe57d1b53781cdeba9da9872fea52639c177bf2
srohimah <sitirohimach@gmail.com>
2018-02-20 17:29:19 +0700

    promise countdown

--- controllers.js
+++ controllers.js
@@ -5,14 +5,19 @@ const timer = seconds =>
     const now = Date.now();
     const then = now + seconds * 1000;
 
-    displayCountdown(seconds - 1);
-    setInterval(() => {
+    //displayCountdown(seconds - 1);
+    let countDown = setInterval(() => {
       const secondsLeft = Math.floor((then - Date.now()) / 1000);
+      let minute = Math.floor(secondsLeft/60)
+      let second = secondsLeft % 60
+      minute = minute < 10 ? '0' + minute : minute
+      second = second < 10 ? '0' + second : second
+      let jam = minute +' : '+second
       if (secondsLeft < 0) {
-        clearInterval();
-        resolve();
+        clearInterval(countDown);
+        resolve(':D');
       } else {
-        displayCountdown(secondsLeft);
+        displayCountdown(jam);
       }
     }, 1000);
   });
